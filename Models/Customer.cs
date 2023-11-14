af9355b1689993e27e7c05967ee09c09c49d042f
Kealan Thal <kealanthal29@gmail.com>
2023-11-14 14:12:41 +0100

    just be sure

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -4,13 +4,13 @@
     {
         public int Id { get; set; }
         
-        public string First_Name { get; set; }
+        public string? First_Name { get; set; }
 
-        public string Last_Name { get; set;}
+        public string? Last_Name { get; set;}
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        public string Gender { get; set; }
+        public string? Gender { get; set; }
 
         public DateTime Birthday { get; set; }
 
