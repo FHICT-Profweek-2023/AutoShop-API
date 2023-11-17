bff1257762ec58d2cd938137824bb28c34b334e4
Kealan Thal <kealanthal29@gmail.com>
2023-11-17 12:17:21 +0100

    Update control model

--- Models/Control.cs
+++ Models/Control.cs
@@ -3,6 +3,11 @@
     public class Control
     {
         public int Id { get; set; }
+
+        public int next_product { get; set; }
+
+        public int current_position { get; set; }
+
         public bool Halt { get; set; }
     }
 }
