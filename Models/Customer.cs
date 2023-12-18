4b5e1712b2c208072ef59a50f886f761706aa6d2
Kealan Thal <kealanthal29@gmail.com>
2023-12-18 13:51:13 +0100

    birthday datetime to string

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -12,7 +12,7 @@
 
         public string? Gender { get; set; }
 
-        public DateTime Birthday { get; set; }
+        public string Birthday { get; set; }
 
         public DateTime Register_Date { get; set; }
     }
