0c232333cef3decf582f8891962e363caedd3e07
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2023-12-20 12:10:38 +0100

    made register_date nullable

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -14,7 +14,7 @@ public class Customer
 
     public string birthday { get; init; }
 
-    public DateTime register_Date { get; init; }
+    public DateTime? register_Date { get; init; }
         
     public string adress { get; init; }
         
